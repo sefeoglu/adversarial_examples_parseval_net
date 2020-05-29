622eedfa769fb5148f120122d36b3ff277e6dd3c
sefeoglu <sefikaefeoglu@gmail.com>
2020-05-29 10:30:38 +0200

    add files
    add resnet structure

---
+++ src/models/wideresnet/resnet.py
@@ -0,0 +1,14 @@
+class WideResNet:
+    def __init__(self):
+        """init the wide resnet"""
+        pass:
+    def build_model(self):
+        """build resnet"""
+        pass:
+
+    def build_train(self):
+        """train model"""
+        pass:
+
+if __name__ == "__main__":
+    pass
